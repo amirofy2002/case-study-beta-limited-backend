7539be1b690ae26841f3b00190ac86463ea1568a
Amir Mehrabi <amir.mehrabi.j@gmail.com>
2025-01-22 00:18:00 +0330

    feat: detect pattern enhanced

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,18 @@ import { AppService } from './app.service';
 import { AppConfigModule } from './configs/app-config.module';
 import { FrameworksModules } from '@frameworks/frameworks.modules';
 import Controllers from './controllers';
+import { UseCasesModule } from '@use-cases/use-cases.module';
+import { APP_FILTER } from '@nestjs/core';
+import { GlobalExceptionFilter } from '@frameworks/global-filter/global.filter';
 @Module({
-  imports: [AppConfigModule, FrameworksModules],
+  imports: [AppConfigModule, FrameworksModules, UseCasesModule],
   controllers: [AppController, ...Controllers],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: GlobalExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
